566f6b0f441e05fb43256db54f8854f59a9bc0e6
Gabriel <gabriel.gsm2017@gmail.com>
2021-12-09 18:47:38 -0300

    comentarios

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-let listaDePedidos = [];
+// é o nosso array onde vai ficar salvo as informações do input 
+let listaDePedidos = []; 
 // let listaDePedidos = [{
 //     nomeDaPizza: 'calabresa',
 //     ingrediente1: 'ovo',
@@ -25,6 +26,7 @@ let listaDePedidos = [];
 // }
 // ]
 
+// demos alguns display none e block para aparecer e sumir a tela quando a gente precisar 
 function cadastrarPizza() {
     let painel1 = document.getElementById('painel-1');
     painel1.style.display='block';  
@@ -36,6 +38,8 @@ function cadastrarPizza() {
     salvarNovo.style.display='block';  
 }
 
+
+// pegamos os id e passamos pra uma variavel
 function salvar(mostrarPainel) {
     let nomePizza1 = document.getElementById('nome-pizza-1');
     let ingrediente1 = document.getElementById('ingrediente-1');
@@ -45,6 +49,9 @@ function salvar(mostrarPainel) {
     let urlImagem = document.getElementById('urlImagem');
     let valorIndice = document.getElementById('indice');
 
+    
+    // criamos um objeto para separar direitinho a pizza dos ingredientes,
+    // e demos um push dessa lista de objeto ao nosso array listaDePedidos la no else mais em baixo 
     const pizza = {
         nomeDaPizza: nomePizza1.value,
         ingrediente1: ingrediente1.value,
@@ -53,14 +60,24 @@ function salvar(mostrarPainel) {
         ingrediente4: ingrediente4.value,
         urlImagem: urlImagem.value
     };
-
-
+    
+    // criamos o if para validar se tem valores nos input, se não o codigo para de executar no if
+    // se tiver correto cai no else e cadastra a pizza normalmente
     if(nomePizza1.value.length < 2 || ingrediente1.value.length < 2 || ingrediente2.value.length < 2 || ingrediente3.value.length < 2 || ingrediente4.value.length < 2 || urlImagem.value.length < 2){
         return alert('precisa preencher todos campos!');
     }else{
         alert('Pizza cadastrada com sucesso!');
     };
-    
+
+    // o map sempre precisa de um return para retornar
+    // se o input valorIndice tiver algum valor é porque é uma edição
+    // criamos uma logica usando o map dentro do if, dentro do if colocamos que se o valor do 
+    // input valorIndice for diferente do input vazio ai cai no map dentro do if,
+    // o primeiro parametro do map vai pegar sempre o objeto ou elemento e o segundo pega
+    // o indice, ai dentro do if colocamos o parseInt para pegar e converter em valor inteiro
+    // ai colocamos se o valor do indice for diferente do indice retornamos o objeto pizza 
+    // se não retornamos o elemento,
+    // se nao cair no if da função damos um push no objeto pizza pro array
     if(valorIndice.value !== ''){
         listaDePedidos = listaDePedidos.map(function(elemento, indice){
             if(parseInt(valorIndice.value) === indice){
@@ -72,8 +89,8 @@ function salvar(mostrarPainel) {
     }else{
         listaDePedidos.push(pizza);
     }
-
-
+    
+    // aqui a gente limpou todos os inputs
     nomePizza1.value = '';
     ingrediente1.value = '';
     ingrediente2.value = '';
@@ -82,37 +99,47 @@ function salvar(mostrarPainel) {
     urlImagem.value = "";    
     valorIndice.value = "";
 
+    // pegamos o id e passamos pra variavel 
     let painel1 = document.getElementById('painel-1');
-
+    
+    // se o parametro morstrarPainel da função salvar for verdadeiro da um display 
+    // block no painel1 se nao da um display none 
     if(mostrarPainel === true){
         painel1.style.display = 'block';
     }else{
         painel1.style.display = 'none';
     }
-}
+}   
 
+// chamamos a função salvar e passamos como parametro o true para salvar e mostrar o formulario
 function salvarNovo() {
     salvar(true);
-}
+} 
 
+ // .length pode ser usado na string e no array
+ // validamos para ver se tem algum item no array
 function pizzasCadastrada() {
     if(listaDePedidos.length === 0 ){
     return alert('Não tem pizza cadastrada')
     }
 
+    // se tiver algum item no array vai fechar o painel1 e abrir o painel2
     let painel2 = document.getElementById('painel-2');
     painel2.style.display='block';
     let painel1 = document.getElementById('painel-1');
     painel1.style.display='none';  
     criarTabela(listaDePedidos);
     mostrarTabela()
+
 }
 
 function filtrar() {
+    // pegamos os valores do input e passamos pra uma variavel
     const inputPizza = document.getElementById('nome-pizza-2').value;       
     const inputIngrediente = document.getElementById('ingredienteP2').value;       
 
-    
+    // fizemos um filtro na listaDePedidos pegando o obj.nomeDaPizza(Variavel do valor do input)
+    // o indexOf serve para fazer uma busca
     const buscarPizza  = listaDePedidos.filter(function(obj){
         const resultado = obj.nomeDaPizza.indexOf(inputPizza);
       
@@ -120,18 +147,25 @@ function filtrar() {
         return resposta;
     });
 
+    // o filter sempre precisa retornar um boolean
+    // fizemos um filtro na listaDePedidos para pesquisar pelos ingredientes
+    // pegando o obj.nomeDoIngrediente(Variavel do valor do input)
+    // o indexOf serve para fazer uma busca e retorna o indice, se achar algo retorna o indice >= 0
+    // se não retorna -1
     const buscarIngrediente = buscarPizza.filter(function(obj){
         const resultadoIngrediente1 = obj.ingrediente1.indexOf(inputIngrediente);
         const resultadoIngrediente2 = obj.ingrediente2.indexOf(inputIngrediente);
         const resultadoIngrediente3 = obj.ingrediente3.indexOf(inputIngrediente);
         const resultadoIngrediente4 = obj.ingrediente4.indexOf(inputIngrediente);
 
+        // se for menor que 0 retorne false se não retorne true
         if(resultadoIngrediente1 < 0 && resultadoIngrediente2 < 0 && resultadoIngrediente3 < 0 && resultadoIngrediente4 < 0){
             return false;
         }else{
            return true;
         }
     });
+    // pegando os ids e passando pra variavel
     const tabela = document.getElementById('tabela');
     const nenhumResultado = document.getElementById('nenhumResultado');
 
@@ -144,9 +178,9 @@ function filtrar() {
         nenhumResultado.style.display="block";        
     }
 
-   
 }
 
+// criamos uma função para criar tabela 
 function criarTabela(dados) {
     const tabela = document.getElementById('tabela');
 
@@ -160,8 +194,7 @@ function criarTabela(dados) {
         <td>Imagem</td>
         <td>Editar</td>
         <td>excluir</td>
-    </tr>`;
-    
+    </tr>`;   
     
     for(let indice in dados) {
         const pedido = dados[indice];
@@ -176,19 +209,22 @@ function criarTabela(dados) {
         <td><button onclick="editarPizza(${indice})">Editar</button></td>
         <td><button onclick="excluirPizza(${indice})">Excluir</button></td>
         </tr>`;
+        console.log(dados)
     }
        
    tabela.innerHTML = conteudoTabela;   
 }
 
-
+//limpamos os inputs
 function limparFiltro() {
     document.getElementById('nome-pizza-2').value = "";       
     document.getElementById('ingredienteP2').value = "";
+    //chamamos a função criarTabela e passamos o parametro do nosso array
     criarTabela(listaDePedidos);         
     mostrarTabela();
 };
 
+// criamos uma função para mostrar tabela
 function mostrarTabela() {
     const tabela = document.getElementById('tabela');
     tabela.style.display='block';    
@@ -197,17 +233,23 @@ function mostrarTabela() {
 }
 
 function excluirPizza(indice) {
+    // confirm serve para mandar uma notificação com resultado positivo ou negativo
+    // retorna false ou true
     const confirmar = window.confirm('Deseja mesmo excluir a pizza cadastrada?')
 
+    // se for igual a true vamos apagar um cadastro de uma pizza pelo indice
     if(confirmar === true){
         listaDePedidos.splice(indice, 1);
        alert('Pizza exluida');
+       //chamamos criarTabela com a lista de pizza no parametro() para atualizar
        criarTabela(listaDePedidos);
     }
 
 }
-
+// criamos uma função para editar 
 function editarPizza(indiceBuscado) {
+    //usamos o find sempre quando queremos buscar apenas um elemento
+    // o find assim como filter precisa de um retorno em bolean
     const pizzaEncontrada = listaDePedidos.find(function(elemento, indice){
         if(indiceBuscado === indice){
             return true;
@@ -216,6 +258,7 @@ function editarPizza(indiceBuscado) {
         }
     });
 
+    // pegamos os valores dos input e demos os valores diferente 
     document.getElementById('nome-pizza-1').value = pizzaEncontrada.nomeDaPizza;
     document.getElementById('ingrediente-1').value = pizzaEncontrada.ingrediente1;
     document.getElementById('ingrediente-2').value = pizzaEncontrada.ingrediente2;
@@ -224,6 +267,7 @@ function editarPizza(indiceBuscado) {
     document.getElementById('urlImagem').value = pizzaEncontrada.urlImagem;
     document.getElementById('indice').value = indiceBuscado;
 
+    // demos alguns display block e display none
     const painel2 = document.getElementById('painel-2');
     painel2.style.display='none';    
     const salvarNovo = document.getElementById('salvar-novo');
